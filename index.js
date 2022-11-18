b3dc69db45331db1d8e4348fe7aad04a9687de76
Ronald Yonggi <ronald.yonggi@ukg.com>
2022-11-18 14:09:34 -0500

    added morgan logging

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 app.use(express.json())
+// app.use(morgan(function (tokens, req, res) {
+//     return [
+//         tokens.method(req, res),
+//         tokens.url(req, res),
+//         tokens.status(req, res),
+//         tokens.res(req, res, 'content-length'), 
+//         '-',
+//         tokens['response-time'](req, res), 'ms',
+//         tokens.res(req, res, 'body')
+//     ].join(' ')
+// }))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
 
 let persons = [
     { 
@@ -76,19 +90,6 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:', request.path)
-    console.log('Body:', request.body)
-    console.log('---')
-    next()
-}
-
-app.use(requestLogger)
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send(P)
-}
 
 const PORT = 3001
 app.listen(PORT)
