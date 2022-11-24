a154145ad8f82a6280e0d4235f7260fa14c38d46
Ronald Yonggi <ronaldyonggi@gmail.com>
2022-11-24 13:11:31 -0500

    static middleware checks for build dir

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 app.use(express.json())
+app.use(express.static('build'))
 // app.use(morgan(function (tokens, req, res) {
 //     return [
 //         tokens.method(req, res),
