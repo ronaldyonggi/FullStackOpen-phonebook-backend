8bd6925ac4cb101e8982b6f3ecaca976af4c9195
Ronald Yonggi <ronaldyonggi@gmail.com>
2022-12-04 11:14:16 -0500

    completed 3.13-3.14, morgan readded

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const morgan = require('morgan')
-// app.use(morgan(function (tokens, req, res) {
-//     return [
-//         tokens.method(req, res),
-//         tokens.url(req, res),
-//         tokens.status(req, res),
-//         tokens.res(req, res, 'content-length'), 
-//         '-',
-//         tokens['response-time'](req, res), 'ms',
-//         tokens.res(req, res, 'body')
-//     ].join(' ')
-// }))
-// morgan.token('body', (req, res) => JSON.stringify(req.body));
-// app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
+const Person = require('./models/person')
+const morgan = require('morgan')
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), 
+        '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.res(req, res, 'body')
+    ].join(' ')
+}))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.use(express.json())
 app.use(cors())
@@ -48,7 +50,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    // response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -62,11 +67,14 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+    // const id = Number(request.params.id)
+    // const person = persons.find(person => person.id === id)
 
-    if (person)response.json(person)
-    else response.status(404).end()
+    // if (person)response.json(person)
+    // else response.status(404).end()
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -84,18 +92,25 @@ app.post('/api/persons', (request, response) => {
     else if (!body.number) return response.status(400).json({
         error: "Number is missing"
     })
-    else if (persons.find(person => person.name === body.name)) return response.status(400).json({
-        error: "Name already exist in the phonebook"
+    // else if (Person.find(person => person.name === body.name)) return response.status(400).json({
+    //     error: "Name already exist in the phonebook"
+    // })
+
+    // const person = {
+    //     name: body.name,
+    //     number: body.number,
+    //     id: Math.floor(Math.random() * 1000)
+    // }
+    const person = new Person({
+        name: body.name,
+        number:body.number
     })
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * 1000)
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
+    // persons = persons.concat(person)
+    // response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 
