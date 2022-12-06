eb8adecfdb6129678f1ed0fd35cd7a9c787fb02f
Ronald Yonggi <ronaldyonggi@gmail.com>
2022-12-06 18:04:25 -0500

    added name validation min 3 chars

--- models/person.js
+++ models/person.js
@@ -12,7 +12,12 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    // name: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
     number: String
 })
 
